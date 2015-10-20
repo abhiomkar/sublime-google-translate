a398fd364cdc6b4d7775a3af1d6e9851008dd069
Abhinay Omkar <abhiomkar@gmail.com>
2015-10-20 21:24:54 +0530

    Update GoogleTranslate.py

--- GoogleTranslate.py
+++ GoogleTranslate.py
@@ -3,7 +3,7 @@
 import sublime, sublime_plugin
 import json
 from googleapiclient.discovery import build
-service = build('translate', 'v2', developerKey='AIzaSyCJZukXZ9hR30mvoDgq2IOwttVLUxlJJpE')
+service = build('translate', 'v2', developerKey='AIzaSyDRRpR3GS1F1_jKNNM9HCNd2wJQyPG3oN0')
 
 class GoogleTranslateCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
