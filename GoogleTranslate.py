998ea2a71ca9344efe7174e15211ff5365d9a719
Abhinay Omkar <abhiomkar@gmail.com>
2015-10-20 21:20:46 +0530

    Initial Commit

---
+++ GoogleTranslate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sublime, sublime_plugin
+import json
+from googleapiclient.discovery import build
+service = build('translate', 'v2', developerKey='AIzaSyCJZukXZ9hR30mvoDgq2IOwttVLUxlJJpE')
+
+class GoogleTranslateCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		result = ''
+		for region in self.view.sel():
+			if not region.empty():
+				v = self.view
+				selection = v.substr(region).encode('utf-8')
+				resp = service.translations().list(source='en', target='de', q=selection.decode()).execute()
+
+				if (resp and len(resp['translations'])):
+					result = resp['translations'][0]['translatedText']
+
+				if result:
+					v.replace(edit, region, result)
